0cc39389358a035e2b3710f51ddb61c36306b278
v-23-69 <devilsadvocate6923@gmail.com>
2025-05-24 23:01:55 +0530

    fix: corrected GitHub repo link

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,9 @@
-// supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '@/integrations/supabase/types' // <-- correct path
 
-const supabase = createClient(
+const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL!,
   import.meta.env.VITE_SUPABASE_ANON_KEY!
-);
+)
 
-export default supabase;
-
+export default supabase
